80404c3e77042f7200588b2fbc0a31c05252e22d
HaoZ124 <zhouhao927@gmail.com>
2022-07-03 13:25:58 -0600

    Finished the basic function

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-const container = document.querySelector('.container');
-
+const button1 = document.querySelector('.setNumber');
+var color = '';
 //Ask user to enter number of boxes on each side
-const numOfBox = document.querySelector('#userInput').value;
+
+
 //Add div to the container
-function createBox(num, div) {
-    var box = document.createElement('div');
-    for (var i = 0; i < num; i++) {
-        for (var j = 0; j < num; j++) {
-            container.appendChild(box);
-        }
-    }
-    div.style.gridTemplateColumns = 'repeat(num, 1fr)';
-    div.style.gridTemplateRows = 'repeat(num, 1fr)';
+function createBox(size) {
+    var container = document.querySelector('.container');
+    var square = container.querySelectorAll('div');
+    square.forEach((div) => div.remove());
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (var i = 0; i < (size ** 2); i++) {
+        var box = document.createElement('div');
+        box.addEventListener('mouseenter', drawing(color));
+        container.insertAdjacentElement('beforeend', box);
+    };
 }
 
-createBox(numOfBox, container);
+function sizing(x) {
+    if (x >= 2 && x <= 64) {
+        createBox(x)
+    } else {
+        alert('Please try again!');
+    }
+}
+
+function drawing(color) {
+    this.style.backgroundColor = 'color';
+}
+
 //Set hovering to the div
